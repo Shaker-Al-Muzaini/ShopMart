00507767224c54ea80d7b6f9b01dce600fe8d134
Shaker-Al-Muzaini <salmazini@students.iugaza.edu.ps>
2025-05-25 01:19:54 +0300

    f1c

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,12 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 });
+//shaker
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
+});
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
