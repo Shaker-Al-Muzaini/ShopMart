51af43171a816949ca54ad347813578217cdd5b9
Shaker-Al-Muzaini <salmazini@students.iugaza.edu.ps>
2025-06-08 23:43:41 +0300

    special_offer

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Inertia\Inertia;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        $best_listProducts =Product::query()->limit(4)->orderBy('sales','desc')->get();
+        $Special_Offers_listProducts=Product::query()->where('is_special_offer', true)->limit(4)->get();
         return inertia::render('Ecommerce/Home', [
             'title' => 'Home',
             'description' => 'Welcome to our e-commerce platform. Explore our products and enjoy a seamless shopping experience.',
