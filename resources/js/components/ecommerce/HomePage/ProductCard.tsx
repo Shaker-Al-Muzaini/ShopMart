6d85cd89cfdbce4732914856433226936289f5ec
Shaker-Al-Muzaini <salmazini@students.iugaza.edu.ps>
2025-06-22 16:47:20 +0300

    d20

--- resources/js/components/ecommerce/HomePage/ProductCard.tsx
+++ resources/js/components/ecommerce/HomePage/ProductCard.tsx
@@ -1,8 +1,11 @@
 import { ProductListItem } from '@/types';
 import { router } from '@inertiajs/react';
 import { Heart, ShoppingBag } from 'lucide-react';
-
-const ProductCard = (product: ProductListItem) => {
+type Props = {
+    product?: ProductListItem;
+};
+const ProductCard = ({ product }: { product: ProductListItem }) => {
+    if (!product) return null;
     const handleDetail = (slug: string) => {
         router.visit(route('product.detail', { slug }));
     };
@@ -14,10 +17,10 @@ const ProductCard = (product: ProductListItem) => {
     return (
         <div className="group overflow-hidden rounded-lg bg-white shadow-sm">
             <div className="relative">
-                <img src={product.image.replace('localhost', '127.0.0.1:8000')} alt="Product" className="h-64 w-full object-cover" />
-                {product.isDiscount && (
-                    <div className="absolute top-0 right-0 m-2 rounded-md bg-red-500 px-2 py-1 text-sm text-white">-{product.discount}%</div>
+                {product?.image && (
+                    <img src={product.image.replace('localhost', '127.0.0.1:8000')} alt="Product" className="h-64 w-full object-cover" />
                 )}
+
                 <div className="bg-opacity-20 absolute inset-0 flex items-center justify-center bg-black/30 opacity-0 transition-opacity group-hover:opacity-100">
                     <button className="mx-2 rounded-full bg-white p-3 text-gray-800 transition hover:bg-indigo-600 hover:text-white">
                         <ShoppingBag className="h-5 w-5" />
@@ -35,7 +38,12 @@ const ProductCard = (product: ProductListItem) => {
                 <div className="flex items-center justify-between">
                     <div>
                         <span className="font-bold text-indigo-600">${product.price}</span>
-                        {product.isDiscount && <span className="ml-2 text-gray-400 line-through">$119.99</span>}
+                        {Number(product.discount) > 0 && (
+                            <div className="absolute top-0 right-0 m-2 rounded-md bg-red-500 px-2 py-1 text-sm text-white">
+                                -{product.discount}%
+                            </div>
+                        )}
+
                     </div>
                     <div className="flex text-yellow-400">
                         <i className="fas fa-star"></i>
